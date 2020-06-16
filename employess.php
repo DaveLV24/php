627e16f540a15b00df651c9097a429e36997c41d
Davis Kronbergs <walk3r@inbox.lv>
2020-06-16 22:12:02 +0300

    editemployess

--- employess.php
+++ employess.php
@@ -2,10 +2,25 @@
 require_once "../config/config.php";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+
+
+$sql = "SELECT id, name, address, salary FROM employees";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  echo "<table><tr><th>ID</th><th>Name</th><th>Address</th><th>Salary</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+     echo "<tr><td>" . $row["id"]. "</td>" . "<td>" . $row["name"]. "</td>" . "<td>" . $row["address"]. "</td>" . "<td>" . $row["salary"]. "</td>" ;
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
