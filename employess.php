a13c840faefe07e69b4264ba62db1157f87228c6
Davis Kronbergs <walk3r@inbox.lv>
2020-06-16 20:52:30 +0300

    editz

---
+++ employess.php
@@ -0,0 +1,11 @@
+<?php
+require_once "../config/config.php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
