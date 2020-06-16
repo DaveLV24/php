0bbe6f037b84c483bbdeddda8db062ca2d7faec9
Davis Kronbergs <walk3r@inbox.lv>
2020-06-17 01:06:11 +0300

    some chnages

--- employess.php
+++ employess.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../config/config.php";
-
+// require_once "../public/scripts/myjs.js";
+// require_once ("../src/templates/header.php");
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -8,17 +9,20 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 
 
 
 $sql = "SELECT id, name, address, salary FROM employees";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-  echo "<table><tr><th>ID</th><th>Name</th><th>Address</th><th>Salary</th></tr>";
+  echo "<table><tr><th>ID</th><th>Name</th><th>Address</th><th>Salary</th><th>Actions</th></tr>";
   // output data of each row
   while($row = $result->fetch_assoc()) {
-     echo "<tr><td>" . $row["id"]. "</td>" . "<td>" . $row["name"]. "</td>" . "<td>" . $row["address"]. "</td>" . "<td>" . $row["salary"]. "</td>" ;
+     echo "<tr><td>" . $row["id"]. "</td>" . "<td>" . $row["name"]. "</td>" . "<td>" . $row["address"]. "</td>" . "<td>" . $row["salary"].  "</td>" . "<td><a class='edit' title='Edit' data-toggle='tooltip'><i class='glyphicon glyphicon-pencil'>Edit;</i></a>
+      <a class='delete' title='Delete' data-toggle='tooltip'><i class='glyphicon glyphicon-trash'>Delete;</i></a>
+        </td>";
+  
   }
   echo "</table>";
 } else {
--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 require_once ("../src/templates/header.php");
 require_once ("../src/templates/loginform.html");
-require_once ("../src/templates/footer.php");
+// require_once ("../src/templates/footer.php");
